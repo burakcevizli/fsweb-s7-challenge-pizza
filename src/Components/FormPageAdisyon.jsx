838b91e463b7d8d4569e56407c6199965f29c326
KrieqqeR <burakcevizli@gmail.com>
2023-08-13 19:27:16 +0300

    Adisyon ve Toplam Güncellendi

--- src/Components/FormPageAdisyon.jsx
+++ src/Components/FormPageAdisyon.jsx
@@ -1,21 +1,22 @@
 import React from "react";
+import { Button, Form, FormGroup, Label, Input, FormText } from "reactstrap";
 
-const FormPageAdisyon = () =>{
+const FormPageAdisyon = () => {
+    
+  return (
+    <div className="AdisyonClass">
+      <h3>Sipariş Toplamı</h3>
+      <div className="Secimler">
+        <h4>Secimler</h4>
+        <h4>25</h4>
+      </div>
+      <div className="Toplam">
+        <h4>Toplam</h4>
+        <h4>100</h4>
+      </div>
+      <Button className="SiparisVer">Sipariş Ver</Button>
+    </div>
+  );
+};
 
-
-    return (
-        <div className="AdisyonClass">
-            <h3>Sipariş Toplamı</h3>
-            <div className="Secimler">
-                <h4>Secimler</h4>
-                <h4>25</h4>
-            </div>
-            <div className="Toplam">
-                <h4>Toplam</h4>
-                <h4>100</h4>
-            </div>
-        </div>
-    )
-}
-
-export default FormPageAdisyon
+export default FormPageAdisyon;
