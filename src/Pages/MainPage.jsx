57df85ddf800491f4ab228d0dcb4aa55d11624f2
KrieqqeR <burakcevizli@gmail.com>
2023-08-17 12:55:14 +0300

    Güncell

--- src/Pages/MainPage.jsx
+++ src/Pages/MainPage.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import "./MainPage.css";
 import { Link } from "react-router-dom";
 import { Button } from "reactstrap";
-import 'bootstrap/dist/css/bootstrap.min.css';
+import "bootstrap/dist/css/bootstrap.min.css";
 
 const MainPage = () => {
   return (
@@ -13,7 +13,9 @@ const MainPage = () => {
           <p className="KodYazı">KOD ACIKTIRIR</p>
           <p className="PızzaYazı">PIZZA DOYURUR</p>
           <Link to="/pizza">
-          <Button color="warning" className="AcıktımButon">ACIKTIM</Button>
+            <Button color="warning" className="AcıktımButon">
+              ACIKTIM
+            </Button>
           </Link>
         </div>
       </div>
